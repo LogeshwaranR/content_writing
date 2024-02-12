151b3440f50939885c868a01f180cdd29c41c11b
Logeshwaran R <logeshminor@gmail.com>
2024-02-12 14:23:28 +0530

    Update content_writing.py

--- content_writing.py
+++ content_writing.py
@@ -13,11 +13,12 @@ with st.sidebar:
   st.header("Sub titles")
   sub_heading = ["Project objective and Goal", "Introduction and Context", "Project Scope- how its implementation will help the organization", "The Project-specific details of the project and its implementation strategy", "Description of the deliverables, the timeframe and estimates of resources for execution", "Conclusion- Key findings & recommendations", "Acknowledgements", "Appendices and References"]
   prompt_inp = []
+  key = os.environ.get('API_KEY')
+  st.write(key)
   for i in sub_heading:
     inp = st.text_input("",i,label_visibility="collapsed")
     prompt_inp.append(inp)
 
-st.write(os.environ['API_KEY'])
 client = OpenAI(api_key = "")
 context = "I am preparing a dissertation on "+usr_title
 
